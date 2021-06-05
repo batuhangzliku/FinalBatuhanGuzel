ea4eb5043069ac9389490421bb5188cbbc132ffc
batuhangzliku <1700002378@stu.iku.edu.tr>
2021-06-05 11:44:38 +0300

    seventh-commit

--- Final/app/Http/Controllers/StudentController.php
+++ Final/app/Http/Controllers/StudentController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Student;
 
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    
+function Save()
+{
+    return view('studentsave');
+}
+
+function save(Request $request)
+{
+   
+    $request->validate([
+        'name' => 'required',
+        'surname' => 'required',
+        'department' => 'required'
+    ]);
+
+    
+    $student = new Student();
+    $student->name = $request->name;
+    $student->surname = $request->surname;
+    $student->department = $request->department;
+    
+    $save = $student->save();
+
+    if ($save) {
+        return back()->with('success', 'successfully');
+    } else {
+        return back()->with('fail', 'Something wrong.');
+    }
+} 
+
 }
